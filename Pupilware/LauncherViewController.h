7f9c086c438ad938293861772f0b9a9e116ca76b
Raymond Martin <rcmartin@smu.edu>
2016-02-12 10:32:52 -0600

    added more linked files

---
+++ Pupilware/LauncherViewController.h
@@ -0,0 +1,13 @@
+//
+//  LauncherViewController.h
+//  CogSense
+//
+//  Created by Sohail Rafiqi on 2/5/15.
+//  Copyright (c) 2015 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LauncherViewController : UIViewController
+
+@end
---
+++ Pupilware/LauncherViewController.m
@@ -0,0 +1,37 @@
+//
+//  LauncherViewController.m
+//  CogSense
+//
+//  Created by Sohail Rafiqi on 2/5/15.
+//  Copyright (c) 2015 SMU. All rights reserved.
+//
+
+#import "LauncherViewController.h"
+
+@interface LauncherViewController ()
+
+@end
+
+@implementation LauncherViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
