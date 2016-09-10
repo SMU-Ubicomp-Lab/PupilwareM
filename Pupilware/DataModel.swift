902de77a8326fa9e09b3adb95d5b39eb004b4007
Chatchai Wangwiwattana <redeian@gmail.com>
2016-09-10 11:40:03 -0500

    add real set of numbersfor target and digit span

--- Pupilware/DataModel.swift
+++ Pupilware/DataModel.swift
@@ -264,14 +264,16 @@ class TargetTest: Test{
     
     func getDigits()->[Int]{
         switch iter{
-        case 0:
-            return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        case 0: // intended for practicing (tutorial)
+            return [1, 2, 3, 4, 5, 1, 7, 8, 9, 10, 11, 7, 13, 14, 15, 16, 17, 18, 19, 20]
         case 1:
-            return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+            return [1, 2, 3, 4, 5, 9, 7, 8, 9, 10, 11, 1, 13, 14, 15, 16, 17, 13, 19, 20]
         case 2:
             return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         case 3:
-            return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+            return [1, 2, 3, 4, 5, 10, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 15, 19, 20]
+        case 4:
+            return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 8, 19, 20]
         default:
             print("TARGET TEST NOT FOUND")
         }
@@ -449,12 +451,30 @@ class DigitTest: Test{
                 
             default:print("DIGIT TEST NOT FOUND")
             }
-        case 6:
-            return getListOfDigits(6)
+        case 6: //use fix number set for consitancy accross participants, and avoid prediable pattern.
+            switch iter{
+            case 1:return [6, 4, 8, 9, 2, 1]
+            case 2:return [3, 2, 3, 7, 1, 0]
+            case 3:return [5, 2, 6, 4, 2, 8]
+            case 4:return [1, 4, 7, 2, 1, 6]
+            default:print("DIGIT TEST NOT FOUND")
+            }
         case 7:
-            return getListOfDigits(7)
+            switch iter{
+            case 1:return [2, 4, 7, 5, 7, 2, 1]
+            case 2:return [4, 3, 6, 8, 0, 9, 7]
+            case 3:return [8, 2, 5, 7, 3, 0, 7]
+            case 4:return [3, 7, 4, 8, 6, 2, 0]
+            default:print("DIGIT TEST NOT FOUND")
+            }
         case 8:
-            return getListOfDigits(8)
+            switch iter{
+            case 1:return [1, 3, 8, 5, 7, 3, 9, 8]
+            case 2:return [5, 2, 4, 7, 0, 6, 3, 8]
+            case 3:return [6, 3, 1, 8, 5, 2, 0, 4]
+            case 4:return [0, 2, 6, 4, 9, 5, 8, 7]
+            default:print("DIGIT TEST NOT FOUND")
+            }
         default:
             print("DIGIT TEST NOT FOUND")
         }
