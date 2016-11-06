54fa56d76de88523ff0b06aa5f24364b174c5aed
Chatchai Wangwiwattana <redeian@gmail.com>
2016-11-06 16:44:17 -0600

    add average intensity

--- Pupilware/PWViewController.mm
+++ Pupilware/PWViewController.mm
@@ -229,10 +229,10 @@
 {
     
     /* Process from a video file, uncomment this block*/
-//    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-//    NSString *documentsDirectory = [paths objectAtIndex:0];
-//    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"v511.mp4"];
-//    [self.videoManager open:filePath];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"v511.mp4"];
+    [self.videoManager open:filePath];
     /*----------------------------------------------------------------------------------------*/
     
     
--- Pupilware/PupilwareCore/Algorithm/PWTypes.hpp
+++ Pupilware/PupilwareCore/Algorithm/PWTypes.hpp
@@ -10,11 +10,19 @@ namespace pw{
     struct PWPupilSize{
         PWPupilSize():leftRadius(0.0f), rightRadius(0.0f){}
         PWPupilSize(float leftRadius, float rightRadius):
-                leftRadius(leftRadius),rightRadius(rightRadius)
+                    leftRadius(leftRadius),rightRadius(rightRadius)
         { }
 
+        PWPupilSize(float leftRadius, float rightRadius, float leftmpi, float rightmpi):
+        leftRadius(leftRadius),rightRadius(rightRadius),
+        leftMPI(leftmpi), rightMPI(rightmpi)
+        { }
+        
         float leftRadius;
         float rightRadius;
+        
+        float leftMPI;
+        float rightMPI;
     };
 
 }
